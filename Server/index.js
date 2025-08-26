0e2144a36bbb163f76282558022ac5c9a9aef73b
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2025-08-26 15:02:17 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"http://localhost:3000" || "https://your-frontend-app.vercel.app",
         credentials:true,
     })
 )
