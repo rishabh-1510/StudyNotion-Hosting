d34563c5a778a76131525fd7e60c5b14922cfc14
Rishabh Bellwal <158290586+rishabh-1510@users.noreply.github.com>
2025-09-06 13:52:30 +0530

    Update ForgotPassword.js

--- src/Pages/ForgotPassword.js
+++ src/Pages/ForgotPassword.js
@@ -21,18 +21,18 @@ const ForgotPassword = () => {
 
      
     return (
-        <div className='text-white flex justify-center items-center flex-col'>
+        <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
             {
                 loading?(
-                    <div> Loading...</div>
+                    <div className="spinner"></div>
                 ):(
-                    <div>
-                        <h1>
+                    <div className="max-w-[500px] p-4 lg:p-8">
+                        <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5">
                             {
                                 !emailSent ? "Reset your Password":"Check your email"
                             }
                         </h1>
-                        <p>
+                        <p className="my-4 text-[1.125rem] leading-[1.625rem] text-richblack-100">
                             {
                                 !emailSent ? "Have no fear. We’ll email you instructions to reset your password. If you dont have access to your email we can try account recovery":
                                 "We have sent the reset email toyouremailaccount@gmail.com"
@@ -41,13 +41,14 @@ const ForgotPassword = () => {
                         <form onSubmit={handleOnSubmit} >
                             {
                                 !emailSent && (
-                                    <label>
-                                        <p>Email Address*</p>
+                                    <label className="w-full">
+                                        <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">Email Address <sup className="text-pink-200">*</sup></p>
                                         <input
                                             required
                                             type='email'
                                             name='email'
                                             value={email}
+                                            className="form-style w-full"
                                             onChange={(e)=>{
                                                 setEmail(e.target.value);
                                             }}
@@ -56,16 +57,17 @@ const ForgotPassword = () => {
                                     </label>
                                 )
                             }
-                            <button>
+                            <button type="submit"
+                                className="mt-6 w-full rounded-[8px] bg-yellow-50 py-[12px] px-[12px] font-medium text-richblack-900">
                                 {
                                     !emailSent ? "Reset Password" :"Resend Email"
                                 }
                             </button>
                         </form>
 
-                        <div>
+                        <div className="mt-6 flex items-center justify-between">
                             <Link to="/login">
-                                <p>Back to Login</p>
+                                <p className="flex items-center gap-x-2 text-richblack-5">Back to Login</p>
                             </Link>
                         </div>
 
