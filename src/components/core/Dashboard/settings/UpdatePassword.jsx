46c98f8e91d149ef715b6d813c4870024464231a
Rishabh Bellwal <158290586+rishabh-1510@users.noreply.github.com>
2025-09-06 13:24:12 +0530

    Update UpdatePassword.jsx

--- src/components/core/Dashboard/settings/UpdatePassword.jsx
+++ src/components/core/Dashboard/settings/UpdatePassword.jsx
@@ -4,6 +4,8 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
+
+
 import { changePassword } from "../../../../services/operations/SettingsAPI"
 import IconBtn from "../../../Common/IconBtn"
 
