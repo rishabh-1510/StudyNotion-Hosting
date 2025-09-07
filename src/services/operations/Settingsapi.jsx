c29ce3f6bc3413e9804ab095704ab81ccc2de11a
Rishabh Bellwal <158290586+rishabh-1510@users.noreply.github.com>
2025-09-07 17:56:30 +0530

    Update Settingsapi.jsx

--- src/services/operations/Settingsapi.jsx
+++ src/services/operations/Settingsapi.jsx
@@ -34,7 +34,8 @@ export function updateDisplayPicture(token, formData) {
         throw new Error(response.data.message)
       }
       toast.success("Display Picture Updated Successfully")
-      dispatch(setUser(response.data.data))
+      dispatch(setUser(response.data.data));
+      localStorage.setItem("user", JSON.stringify(response.data.data));
     } catch (error) {
       console.log("UPDATE_DISPLAY_PICTURE_API API ERROR............", error)
       toast.error("Could Not Update Display Picture")
